b1b944ccc6705d550d66c63d5e457d2fbb6cc65c
Lucifer Locas <rohitsomvanshi2003@gmail.com>
2023-10-17 15:30:22 +0530

    menu toggle

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 "use strict";
 
-const navbarMenu = document.querySelector(".navbar .links");
-const menuBtn = document.querySelector(".menu-btn");
-// const hideMenuBtn = navbarMenu.querySelector(".close-btn");
+const navbarMenu1 = document.getElementById("hamNav");
+const navbarMenu2 = document.getElementById("links");
+// console.log(navbarMenu1);
+// console.log(navbarMenu2);
+const menuBtn = document.getElementById("menu-btn");
+const hideMenuBtn = navbarMenu1.getElementById("close-btn");
 
 // menuBtn.addEventListener("click", () => {
-//   navbarMenu.classList.toggle("show-menu");
+//   navbarMenu1.classList.toggle("show-menu");
+// });
+// navbarMenu1.getElementById("hamNav").addEventListener("click", () => {
+//   navbarMenu1.classList.toggle("show-menu");
 // });
 
+navbarMenu1.querySelector("#hamNav").addEventListener("click", () => {
+  alert("Clicked qs");
+});
+
 // hideMenuBtn.addEventListener("click", () => {
-//   navbarMenu.classList.toggle("show-menu");
+//   navbarMenu1.classList.toggle("show-menu");
 // });
+
+// document.getElementById("hamNav").addEventListener("click", () => {
+//   alert("Clicked");
+// });
+
+// hiding everthing till load-----------------------------------------
 
 document.onreadystatechange = function () {
   if (document.readyState !== "complete") {
@@ -21,6 +37,8 @@ document.onreadystatechange = function () {
     document.querySelector("body").style.visibility = "visible";
   }
 };
+
+// fetching API and extracting data from it----------------------------
 
 fetch("https://kontests.net/api/v1/all")
   .then((res) => {
@@ -45,14 +63,46 @@ fetch("https://kontests.net/api/v1/all")
     // alert("Error fetching the API");
   });
 
+// downloading the API data table---------------------------------------
+
+function htmlToCsv(filename) {
+  var data = [];
+  var rows = document.querySelectorAll("table tr");
+
+  for (var i = 0; i < rows.length; i++) {
+    var row = [],
+      cols = rows[i].querySelectorAll("td, th");
+
+    for (var j = 0; j < cols.length; j++) {
+      row.push(cols[j].innerText);
+    }
+    data.push(row.join(","));
+  }
+  downloadCSVFile(data.join("\n"), filename);
+}
+
+function downloadCSVFile(csv, filename) {
+  var csv_file, download_link;
+
+  csv_file = new Blob([csv], { type: "text/csv" });
+  download_link = document.createElement("a");
+  download_link.download = filename;
+  download_link.href = window.URL.createObjectURL(csv_file);
+  download_link.style.display = "none";
+  document.body.appendChild(download_link);
+  download_link.click();
+}
+
+// making search enable for the competitions------------------------------
+
 const search = () => {
   const searchbox = document.getElementById("search-name").value.toUpperCase();
   const platforms = document.getElementById("code");
   const information = document.querySelectorAll(".info");
-  const pname = platforms.getElementsByClassName("plat");
+  const pname = platforms.getElementById("plat");
 
   for (var i = 0; i < pname.length; i++) {
-    let match = information[i].getElementsByClassName("plat")[0];
+    let match = information[i].getElementById("plat")[0];
 
     if (match) {
       let textValue = match.textContent || match.innerHTML;
@@ -66,52 +116,49 @@ const search = () => {
   }
 };
 
+// pagination but not working properly -------------------------------------
+
 document.addEventListener("DOMContentLoaded", function () {
-  const content = document.querySelector("#inform");
-  const itemsPerPage = 6;
-  let currentPage = 0;
-  const items = Array.from(content.getElementsByTagName("tr")).slice(1);
-
-  function showPage(page) {
-    const startIndex = page * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    items.forEach((item, index) => {
-      item.classList.toggle("hidden", index < startIndex || index >= endIndex);
-    });
-    updateActiveButtonStates();
-  }
-
-  function createPageButtons() {
-    const totalPages = Math.ceil(items.length / itemsPerPage);
-    const paginationContainer = document.createElement("div");
-    const paginationDiv = document.body.appendChild(paginationContainer);
-    paginationContainer.classList.add("pagination");
-
-    for (let i = 0; i < totalPages; i++) {
-      const pageButton = document.createElement("button");
-      pageButton.textContent = i + 1;
-      pageButton.addEventListener("click", () => {
-        currentPage = i;
-        showPage(currentPage);
-        updateActiveButtonStates();
-      });
-
-      content.appendChild(paginationContainer);
-      paginationDiv.appendChild(pageButton);
-    }
-  }
-
-  function updateActiveButtonStates() {
-    const pageButtons = document.querySelectorAll(".pagination button");
-    pageButtons.forEach((button, index) => {
-      if (index === currentPage) {
-        button.classList.add("active");
-      } else {
-        button.classList.remove("active");
-      }
-    });
-  }
-
-  createPageButtons();
-  showPage(currentPage);
+  // const content = document.querySelector("#code");
+  // console.log(content);
+  // const itemsPerPage = 6;
+  // let currentPage = 0;
+  // const items = Array.from(content.getElementsByTagName("tr")).slice(1);
+  // function showPage(page) {
+  //   const startIndex = page * itemsPerPage;
+  //   const endIndex = startIndex + itemsPerPage;
+  //   items.forEach((item, index) => {
+  //     item.classList.toggle("hidden", index < startIndex || index >= endIndex);
+  //   });
+  //   updateActiveButtonStates();
+  // }
+  // function createPageButtons() {
+  //   const totalPages = Math.ceil(items.length / itemsPerPage);
+  //   const paginationContainer = document.createElement("div");
+  //   const paginationDiv = document.body.appendChild(paginationContainer);
+  //   paginationContainer.classList.add("pagination");
+  //   for (let i = 0; i < totalPages; i++) {
+  //     const pageButton = document.createElement("button");
+  //     pageButton.textContent = i + 1;
+  //     pageButton.addEventListener("click", () => {
+  //       currentPage = i;
+  //       showPage(currentPage);
+  //       updateActiveButtonStates();
+  //     });
+  //     content.appendChild(paginationContainer);
+  //     paginationDiv.appendChild(pageButton);
+  //   }
+  // }
+  // function updateActiveButtonStates() {
+  //   const pageButtons = document.querySelectorAll(".pagination button");
+  //   pageButtons.forEach((button, index) => {
+  //     if (index === currentPage) {
+  //       button.classList.add("active");
+  //     } else {
+  //       button.classList.remove("active");
+  //     }
+  //   });
+  // }
+  // createPageButtons();
+  // showPage(currentPage);
 });
